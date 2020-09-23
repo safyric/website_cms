bb5c3038a8269a8c40e1801c671fe79019f3eee9
jchanme <39323414+jchanme@users.noreply.github.com>
2020-09-23 22:08:15 +0800

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Website CMS',
+    'version': '1.0',
+    'summary': 'Content management for website',
+    'website': 'https://www.safyric.com',
+    'category': 'Website',
+    'description': """
+""",
+    'depends': [
+        'website'
+    ],
+    'data': [
+        'views/category.xml',
+        'views/standards.xml',
+        'views/product_data.xml',
+        'views/product_feature.xml',
+        'templats/website.xml'
+    ],
+    'installable': True,
+}
