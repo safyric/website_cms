588098717decb0b0a83cd71a73a5aa9bca418cd0
jchanme <39323414+jchanme@users.noreply.github.com>
2020-09-24 22:20:16 +0800

    Update cms.py

--- models/cms.py
+++ models/cms.py
@@ -2,10 +2,43 @@
 
 from odoo import models, fields
 
-class CMSStandards(models.Model):
-    _name = 'cms.standards'
-    _description = 'Standards'
+class cms_category(models.Model):
+    _name = 'cms.category'
+    _description = 'CMS Category'
+    
+    name = fields.Char(string='name', required=True, translate=True)
+    description = fields.Text(string='Description', translate=True)
+
+class cms_brief(models.Model):
+    _name = 'cms.brief'
+    _description = 'Brief'
+    
+    name = fields.Char(string='Name', required=True, translate=True)
+    description = fields.Char(string='Description', translate=True)
+    cms_id = fields.Many2one('cms.cms', 'CMS ID')
+
+class cms_standard(models.Model):
+    _name = 'cms.standard'
+    _description = 'Standard'
 
     name = fields.Char(string="Name", required=True)
     description = fields.Char(string="Descriptin", translate=True)
-    url = fields.Char(string="URL")
+    url = fields.Char(string='URL')
+    cms_id = fields.Many2one('cms.cms', 'CMS ID')
+    standard_category_ids = fields.One2many('cms.standard.category', 'standard_id', string='Category')
+
+class cms_standard_category(models.Model):
+    _name = 'cms.standard.category'
+    _description = 'Standard Categories'
+    
+    name = fiels.Char(string="Name", required=True, translate=True)
+    standard_id = fields.Many2one('cms.standard', 'Standard ID')
+
+class cms_cms(models.Model):
+    _name = 'cms.cms'
+    _descriptoin = 'CMS'
+    
+    name = fields.Char(string="Name" required=True)
+    category_id = fields.Many2one('cms.category', string='Category')
+    brief_ids = fields.One2many('cms.brief', 'cms_id', string='Breif Lines')
+    standard_ids = fields.One2many('cms.standard', 'cms_id', string='Standards')
